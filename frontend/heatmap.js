3c55d40f7c04f6648bad9b8d1e3b59cf495c33d3
Sylwia <sylwia.kucharska11@gmail.com>
2019-08-07 10:04:37 +0200

    Scale, tooltip and legend values globally defined

--- frontend/heatmap.js
+++ frontend/heatmap.js
@@ -7,9 +7,18 @@ var margin = {top: 30, right: 50, bottom: 150, left: 90};
 var colors = ["#b2192d", "#d91e36", "#d7656e","#d5beb3", "#d4f4dd", "#75d9cc","#17bebb","#0e7c7b"];
 var months = ["January","February","March","April","May","June","July", "August","September","October","November","December", ""]
 var xTranslate = height -margin.bottom;
+var legend_marg = margin.bottom - margin.top;
+var legend_rect_height = 20;
+var legend_rect_width = legend_rect_height * 1.618;
+
+var xScale = d3.scaleLinear().range([margin.left, width - margin.right]);
+var yScale = d3.scaleBand().range([margin.top, height -margin.bottom]);
+var xAxis = d3.axisBottom().scale(xScale).tickFormat(d3.format("d"));
+var yAxis = d3.axisLeft().scale(yScale).tickSize(0);
+var tempScale = d3.scaleLinear().range([ colors.length -1, 0]);
+
 var dropdown = d3.selectAll("#d3-dropdown");
-
-
+var tooltip = d3.selectAll("body").append("div").attr("id", "tooltip");
 ///////////////////////////////////////////////////////////////////////////////////
 // functions
 function update_paragraph_text(id, text = "Z-Score For Monthly Temperature", padding_left=margin.left){
@@ -52,6 +61,18 @@ function z_standarize(value, m, monthly_mean_temp, monthly_sd_temp){
   return (value - monthly_mean_temp[month]) / monthly_sd_temp[month]
 }
 
+function display_tooltip(html_text){
+  tooltip
+    .style("left", d3.event.pageX -margin.right + "px")
+    .style("top", d3.event.pageY - margin.left + "px")
+    .style("display", "inline-block")
+    .html(html_text);
+}
+
+function hide_tooltip(d){
+  tooltip.style("display", "none");
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////
 // initial chart
@@ -76,17 +97,9 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
 
   let months_unique = Array.from(new Set(yMonths)).sort(function(a,b) { return a - b; });
 
-  var xScale = d3.scaleLinear()
-    .domain([d3.min(xYears), d3.max(xYears) +1])
-    .range([margin.left, width - margin.right]);
-  var yScale = d3.scaleBand()
-    .domain(months_unique)
-    .range([margin.top, height -margin.bottom]);
-  var xAxis = d3.axisBottom().scale(xScale)
-    .tickFormat(d3.format("d"));
-  var yAxis = d3.axisLeft().scale(yScale)
-    .tickFormat((d, i)=> months[i])
-    .tickSize(0);
+  xScale.domain([d3.min(xYears), d3.max(xYears) +1]);
+  yScale.domain(months_unique);
+  yAxis.tickFormat((d, i)=> months[i]);
 
   svg.append("g").call(xAxis).attr("id", "x_axis")
     .attr("transform", "translate(0," + xTranslate + ")").attr("id", "x-axis");
@@ -102,9 +115,7 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
   var monthly_sd_temp = calc_monthly_temp_sd(monthly_temp_arrays(data, months_unique));
 
   var temp = data.map((d)=>parseFloat(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)));
-  var tempScale = d3.scaleLinear()
-    .domain([d3.min(temp), d3.max(temp)])
-    .range([ colors.length -1, 0]);
+  tempScale.domain([d3.min(temp), d3.max(temp)]);
 
   var sqr_height = (height - margin.bottom - margin.top) / (d3.max(yMonths) - d3.min(yMonths) +1)
   var sqr_width = (width - margin.left - margin.right) / (d3.max(xYears) - d3.min(xYears)+ 1)
@@ -121,14 +132,11 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
     .attr("data-month", (d) => d["month"] -1)
     .attr("data-year", (d) => d["year"])
     .attr("data-temp", (d) => z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp))
-    .attr("fill",(d) => colors[parseInt(tempScale(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)))]);
+    .attr("fill",(d) => colors[parseInt(tempScale(z_standarize(d["t"], d["month"],
+     monthly_mean_temp, monthly_sd_temp)))]);
 
   //-----------------------------------------
   // 4. LEGEND
-  var marg = margin.bottom - margin.top
-  var rect_height = 20;
-  var rect_width = rect_height * 1.618;
-
   var legend = svg.selectAll("#legend")
     .data(colors)
     .enter()
@@ -139,39 +147,29 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
     .append("rect")
     .attr("fill", (d, i)=>d)
     .attr("class", "legend_rect")
-    .attr("x", (d, i)=> width -margin.left - i * rect_width)
-    .attr("y", height - marg)
-    .attr("width", rect_width)
-    .attr("height", rect_height);
+    .attr("x", (d, i)=> width -margin.left - i * legend_rect_width)
+    .attr("y", height - legend_marg)
+    .attr("width", legend_rect_width)
+    .attr("height", legend_rect_height);
 
   legend
     .append("text")
     .text((d, i)=>d3.format("0.2n")(tempScale.invert(i)))
-    .attr("x", (d, i)=> width -margin.left - i * rect_width + rect_width /2)
-    .attr("y", height - marg + rect_width)
+    .attr("x", (d, i)=> width -margin.left - i * legend_rect_width + legend_rect_width /2)
+    .attr("y", height - legend_marg + legend_rect_width)
     .attr("text-anchor", "middle");
 
   //-----------------------------------------
   // 5. TOOLTIP
-  var tooltip = d3.selectAll("body")
-    .append("div")
-    .attr("id", "tooltip");
-
   d3.selectAll(".cell")
     .on("mouseover", function(d){
-      tooltip
-        .style("left", d3.event.pageX -margin.right + "px")
-        .style("top", d3.event.pageY - margin.left + "px")
-        .style("display", "inline-block")
-        .html("Date: " + d["year"] + ", " + months[d["month"] - 1] + "<br>Temp: " + d3.format(".3")(d["t"]) + "<br>Deviation:" + d3.format(".3")(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)) + "℃");
-
-      d3.select(this).style("stroke-width", 2)
-      .style("stroke", "black");
-  })
+      html_text = "Date: " + d["year"] + ", " + months[d["month"] - 1] + "<br>Temp: " + d3.format(".3")(d["t"]) + "<br>Deviation:" + d3.format(".3")(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)) + "℃"
+      display_tooltip(html_text)
+      d3.select(this).style("stroke-width", 2).style("stroke", "black");
+    })
     .on("mouseout", function(d){
-      tooltip.style("display", "none");
-    d3.select(this).style("stroke-width", 0);
-  });
+      hide_tooltip(d)
+      d3.select(this).style("stroke-width", 0);});
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -183,7 +181,7 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
       d3.csv(selected).then(function(data){
         var xYears = data.map((d)=>parseInt(d["year"]));
         var yMonths = data.map((d)=>parseInt(d["month"]) -1);
-
+        let months_unique = Array.from(new Set(yMonths)).sort(function(a,b) { return a - b; });
         //-----------------------------------------
         // 1. TITLE & SVG
         update_paragraph_text("#description", text = "Calculated for " + d3.min(xYears) + " - " +  + d3.max(xYears))
@@ -195,19 +193,10 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
 
         //-----------------------------------------
         // 2. SCALE & AXIS
-        let months_unique = Array.from(new Set(yMonths)).sort(function(a,b) { return a - b; });
 
-        var xScale = d3.scaleLinear()
-          .domain([d3.min(xYears), d3.max(xYears) +1])
-          .range([margin.left, width - margin.right]);
-        var yScale = d3.scaleBand()
-          .domain(months_unique)
-          .range([margin.top, height -margin.bottom]);
-        var xAxis = d3.axisBottom().scale(xScale)
-          .tickFormat(d3.format("d"));
-        var yAxis = d3.axisLeft().scale(yScale)
-          .tickFormat((d, i)=> months[i])
-          .tickSize(0);
+        xScale.domain([d3.min(xYears), d3.max(xYears) +1]);
+        yScale.domain(months_unique);
+        yAxis.tickFormat((d, i)=> months[i]);
 
         svg.append("g").call(xAxis)
           .attr("transform", "translate(0," + xTranslate + ")").attr("id", "x-axis");
@@ -223,9 +212,7 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
           var monthly_sd_temp = calc_monthly_temp_sd(monthly_temp_arrays(data, months_unique));
 
           var temp = data.map((d)=>parseFloat(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)));
-          var tempScale = d3.scaleLinear()
-            .domain([d3.min(temp), d3.max(temp)])
-            .range([ colors.length -1, 0]);
+          tempScale.domain([d3.min(temp), d3.max(temp)]);
 
           var sqr_height = (height - margin.bottom - margin.top) / (d3.max(yMonths) - d3.min(yMonths) +1)
           var sqr_width = (width - margin.left - margin.right) / (d3.max(xYears) - d3.min(xYears)+ 1)
@@ -246,9 +233,6 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
 
           //-----------------------------------------
           // 4. LEGEND
-          var marg = margin.bottom - margin.top
-          var rect_height = 20;
-          var rect_width = rect_height * 1.618;
 
           var legend = svg.selectAll("#legend")
             .data(colors)
@@ -260,39 +244,30 @@ d3.csv("preprocessed_csv/stockholm_mean.csv")
             .append("rect")
             .attr("fill", (d, i)=>d)
             .attr("class", "legend_rect")
-            .attr("x", (d, i)=> width -margin.left - i * rect_width)
-            .attr("y", height - marg)
-            .attr("width", rect_width)
-            .attr("height", rect_height);
+            .attr("x", (d, i)=> width -margin.left - i * legend_rect_width)
+            .attr("y", height - legend_marg)
+            .attr("width", legend_rect_width)
+            .attr("height", legend_rect_height);
 
           legend
             .append("text")
             .text((d, i)=>d3.format("0.2n")(tempScale.invert(i)))
-            .attr("x", (d, i)=> width -margin.left - i * rect_width + rect_width /2)
-            .attr("y", height - marg + rect_width)
+            .attr("x", (d, i)=> width -margin.left - i * legend_rect_width + legend_rect_width /2)
+            .attr("y", height - legend_marg + legend_rect_width)
             .attr("text-anchor", "middle");
 
           //-----------------------------------------
           // 5. TOOLTIP
-          var tooltip = d3.selectAll("body")
-            .append("div")
-            .attr("id", "tooltip");
-
           d3.selectAll(".cell")
             .on("mouseover", function(d){
-              tooltip
-                .style("left", d3.event.pageX -margin.right + "px")
-                .style("top", d3.event.pageY - margin.left + "px")
-                .style("display", "inline-block")
-                .html("Date: " + d["year"] + ", " + months[d["month"] - 1] + "<br>Temp: " + d3.format(".3")(d["t"]) + "<br>Deviation:" + d3.format(".3")(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)) + "℃");
-
-              d3.select(this).style("stroke-width", 2)
-              .style("stroke", "black");
-          })
+              html_text = "Date: " + d["year"] + ", " + months[d["month"] - 1] + "<br>Temp: " + d3.format(".3")(d["t"]) + "<br>Deviation:" + d3.format(".3")(z_standarize(d["t"], d["month"], monthly_mean_temp, monthly_sd_temp)) + "℃"
+              display_tooltip(html_text)
+              d3.select(this).style("stroke-width", 2).style("stroke", "black");
+            })
             .on("mouseout", function(d){
-              tooltip.style("display", "none");
-            d3.select(this).style("stroke-width", 0);
-          });
+              hide_tooltip(d)
+              d3.select(this).style("stroke-width", 0);});
+        });
 
       });
 
